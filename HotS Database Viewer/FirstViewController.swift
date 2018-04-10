ec07f9ebcd79e67f7ec96a5d54808cfb18392de5
Student <student@howard06.istlabs.rit.edu>
2018-04-09 22:34:37 -0400

    Fill project directory

---
+++ HotS Database Viewer/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  HotS Database Viewer
+//
+//  Created by Student on 4/9/18.
+//  Copyright © 2018 Jordan Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ HotS Database Viewer/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  HotS Database Viewer
+//
+//  Created by Student on 4/9/18.
+//  Copyright © 2018 Jordan Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
